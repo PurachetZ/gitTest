91b6253b5cc3606b13d2737fe85964e3a83ff6a8
Purachet <poorachad2545@gmail.com>
2020-10-20 18:19:07 +0700

    new

--- src/Gittest/Gittest.java
+++ src/Gittest/Gittest.java
@@ -12,9 +12,14 @@ package Gittest;
 public class Gittest {
     public static void main(String[] args) {
         sayHello();
+        sayGoodBye();
     }
     
     public static void sayHello(){
         System.out.println("Hello");
+    }
+    
+    public static void sayGoodBye(){
+        System.out.println("Goodbye");
     }
 }
